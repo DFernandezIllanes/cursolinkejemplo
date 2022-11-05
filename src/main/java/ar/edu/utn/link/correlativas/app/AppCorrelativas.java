43caea2005adfc61c20701f5fa4560398deeb321
Daniel <dfernandez2993@gmail.com>
2022-11-05 10:30:10 -0300

    Agregado AppCorrelativas(SBApp) y dependencias SBDevTools y Lombok

---
+++ src/main/java/ar/edu/utn/link/correlativas/app/AppCorrelativas.java
@@ -0,0 +1,12 @@
+package ar.edu.utn.link.correlativas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCorrelativas {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCorrelativas.class, args);
+    }
+}
